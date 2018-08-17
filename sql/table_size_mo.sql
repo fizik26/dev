ffd5c820223ded14b461cca65c7985e2aedf9cc5
Thibaut Bouldoire <thibaut.bouldoire@gmail.com>
2018-08-17 15:57:13 +0200

    Update table_size_mo.sql

--- sql/table_size_mo.sql
+++ sql/table_size_mo.sql
@@ -1,3 +1,3 @@
-// return the size in mo of your selected table ( parameter tablename )
+# return the size in mo of your selected table ( parameter tablename )
 
 SELECT CONCAT(ROUND(((cast(DATA_LENGTH as signed ) + cast(INDEX_LENGTH as signed )) / 1024 / 1024), 2), 'Mo') AS TailleMo FROM information_schema.TABLES WHERE table_name = 'tableName';
