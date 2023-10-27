5225d02658a15e8754a75e995113abe789ad87ea
Thibaut Bouldoire <thibaut.bouldoire@gmail.com>
2023-10-27 12:34:19 +0300

    Add files via upload
    Add files on directory for Changing User infos ( roles and other ) on a test file with Spring Boot and JUnit

---
+++ Java/change_role_spring_test/TestSecurityContextBuilder.java
@@ -0,0 +1,37 @@
+package com.example.folder_sans_rapport.changeUserRoleOnAUnitTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class TestSecurityContextBuilder {
+
+    private TestSecurityContextBuilder() {
+
+    }
+
+    public static SecurityContext withRoleAndUserName(Roles role, String userName) {
+        return getContextForUserRights(role, userName);
+    }
+
+    private static SecurityContext getContextForUserRights(Roles role, String userName) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        DemoUser demoUser = getDemoUser(role, userName);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(demoUser, "password",
+                new ArrayList<>(demoUser.getAuthorities()));
+        context.setAuthentication(auth);
+        return context;
+    }
+
+    private static DemoUser getDemoUser(Roles role, String userName) {
+        List<String> authorities = new ArrayList<>();
+        authorities.add(role.getRole());
+
+        return new DemoUser(userName, authorities);
+    }
+}
---
+++ Java/change_role_spring_test/changeUserRoleOnAUnitTest.java
@@ -0,0 +1,53 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.security.test.context.*;
+
+folder_sans_rapport.changeUserRoleOnAUnitTest.Roles;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import java.sql.Connection;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class changeUserRoleOnAUnitTest {
+    
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void configure() {
+        this.mockMvc = webAppContextSetup(this.webApplicationContext).build();
+
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            // example of execution of SQL script for the tests
+            ScriptUtils.executeSqlScript(connection, new ClassPathRessource("/path_scripts_sql"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @WithMockUser(authorities = Roles.ROLE_3)
+    public void testMethodToTestSomething() {
+        // do tests
+
+        // change user role and set why not a new user name
+        TestSecurityContextHolder.setContext(TestSecurityContextBuilder.withRoleAndUserName(Roles.ROLE_2, "212151"));
+
+        // now the user has a new role and maybe a new username
+    }
+}
